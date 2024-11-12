193868e3e92df8bcd4f6f82a5840584c5cdb05b6
Fredy Antonio Pelaez C <103303538+Fredy-AP@users.noreply.github.com>
2024-11-11 19:59:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function displayMessage() {
+    const messageElement = document.getElementById("message");
+    messageElement.textContent = "Lina Esperanza Restrepo, te amo";
+    messageElement.style.opacity = 1;
+}
